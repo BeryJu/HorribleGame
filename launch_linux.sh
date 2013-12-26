02a812caba23170d47cd844c682a0bef53f637bd
BeryJu <jenslanghammer@gmail.com>
2013-12-26 17:19:23 +0100

    Fully fixed it ^ 2

--- launch_linux.sh
+++ launch_linux.sh
@@ -3,9 +3,8 @@
 # @Date:   2013-12-26 16:32:04
 # @Email:  jenslanghammer@gmail.com
 # @Last Modified by:   BeryJu
-# @Last Modified time: 2013-12-26 17:08:15
+# @Last Modified time: 2013-12-26 17:18:27
 mkdir -p "nw/linux-64"
-cd "nw/linux-64"
 FILE="nw"
 pwd
 
@@ -13,6 +12,7 @@ if [ -f $FILE ];
 then
 	nw/linux-64/nw bin/
 else
+	cd "nw/linux-64"
 	wget "http://s3.amazonaws.com/node-webkit/v0.8.3/node-webkit-v0.8.3-linux-x64.tar.gz"
 	tar xvzf "node-webkit-v0.8.3-linux-x64.tar.gz"
 	cd "node-webkit-v0.8.3-linux-x64"
