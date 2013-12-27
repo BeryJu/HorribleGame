0a7efc57a8773e16bc00c4419fdc803b0ca9e540
BeryJu <jenslanghammer@gmail.com>
2013-12-27 20:16:23 +0100

    Fixed ChasingCameraEntity

--- src/HG/Entities/ChasingCameraEntity.ts
+++ src/HG/Entities/ChasingCameraEntity.ts
@@ -3,7 +3,7 @@
 * @Date:   2013-11-06 14:36:09
 * @Email:  jenslanghammer@gmail.com
 * @Last Modified by:   BeryJu
-* @Last Modified time: 2013-12-26 15:41:18
+* @Last Modified time: 2013-12-27 20:14:34
 */
 
 module HG.Entities {
@@ -29,6 +29,7 @@ module HG.Entities {
 		}
 
 		frame(delta: number): void {
+			this._target.object.updateMatrixWorld(true);
 			var cameraOffset = this.positionOffset.clone().
 					applyMatrix4(this._target.object.matrixWorld);
 			this.object.position = cameraOffset;
